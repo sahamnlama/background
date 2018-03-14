52a1115c6e3c3507462f40cb0757e99e1caee934
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-14 15:58:19 +0530

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -9,23 +9,7 @@ function onDeviceReady() {
 
 
       // 1) Request background execution
-      cordova.plugins.backgroundMode.enable();
 
-      // 2) Now the app runs ins background but stays awake
-      cordova.plugins.backgroundMode.on('activate', function () {
-          setInterval(function () {
-              cordova.plugins.notification.badge.increase();
-          }, 1000);
-      });
-
-      // 3) App is back to foreground
-      cordova.plugins.backgroundMode.on('deactivate', function () {
-          cordova.plugins.notification.badge.clear();
-      });
-
-alert(cordova.plugins.backgroundMode.isActive());
-
-      alert('ssss');
 
     // Add similar listeners for other events
 }
