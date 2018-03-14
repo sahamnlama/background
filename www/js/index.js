65191e669a2e4536e5fe961efbdde51cb1e3a604
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-14 17:12:06 +0530

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -5,13 +5,6 @@ function onLoad() {
 // device APIs are available
 //
 function onDeviceReady() {
-
-
-alert();
-      // 1) Request background execution
-      //cordova.plugins.backgroundMode.enable();
-      alert(cordova.plugins.backgroundMode.isActive());
-
-
-    // Add similar listeners for other events
+    var aa = cordova.plugins.backgroundMode.isActive();
+    document.getElementById('aaa').value = aa;
 }
