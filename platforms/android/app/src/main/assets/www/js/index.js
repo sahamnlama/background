10a62a448f2d4746bd23df59b910023cb5295dd1
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-14 17:17:27 +0530

    first commit

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -5,6 +5,12 @@ function onLoad() {
 // device APIs are available
 //
 function onDeviceReady() {
+
+  cordova.plugins.backgroundMode.setDefaults({
+    title: 'sdsdsdsd',
+    text: 'dsfsdfdsfsdfsdf'
+})
     var aa = cordova.plugins.backgroundMode.isActive();
+    alert(aa);
     document.getElementById('aaa').value = aa;
 }
