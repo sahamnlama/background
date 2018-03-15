38de6d4106f872cb88eff0722b26964c8aa8abfb
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-15 10:06:28 +0530

    first commit

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -10,6 +10,26 @@ function aaa1(){
 //
 function onDeviceReady() {
 
+  // Enable background mode
+  cordova.plugins.backgroundMode.enable();
+  // Android customization
+  cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+
+  cordova.plugins.backgroundMode.onfailure = function(errorCode) {};
+
+  // Called when background mode has been activated
+  cordova.plugins.backgroundMode.onactivate = function () {
+
+  setTimeout(function () {
+  // Modify the currently displayed notification
+    cordova.plugins.backgroundMode.configure({
+        text:'Running in background for more than 5s now.',
+        hidden: false,
+        resume: true
+      });
+  }, 10000);
+  }
+
   //cordova.InAppBrowser.open('http://192.168.1.6:7777', '_self', 'location=yes,hidden=no');
   var conn = new WebSocket('ws://192.168.1.6:7777');
 
@@ -24,39 +44,20 @@ function onDeviceReady() {
 
   conn.onmessage = function (msg) {
     console.log("Got message", msg.data);
-    alert(msg.data);
+    //alert(msg.data);
     cordova.plugins.notification.local.schedule({
         title: 'FirstMessage',
         text: msg.data,
-        foreground: true,
-        vibrate:true
-
+        foreground: true
     });
+
+    cordova.plugins.backgroundMode.configure({
+        text:'Running in background for more than 5s now.',
+        hidden: false,
+        resume: true
+      });
   }
       //var ref =
 /*
-      // Enable background mode
-      cordova.plugins.backgroundMode.enable();
-      // Android customization
-      cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
-
-      cordova.plugins.backgroundMode.onfailure = function(errorCode) {};
-
-      // Called when background mode has been activated
-      cordova.plugins.backgroundMode.onactivate = function () {
-      cordova.plugins.notification.local.schedule({
-          title: 'My first notification111',
-          text: 'Thats pretty easy111...',
-          foreground: false
-      });
-
-      setTimeout(function () {
-      // Modify the currently displayed notification
-        cordova.plugins.backgroundMode.configure({
-            text:'Running in background for more than 5s now.',
-            hidden: false,
-            resume: true
-          });
-      }, 10000);
-    }*/
+      */
 }
